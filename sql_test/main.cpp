65e93ba84ed08024e0adb1a347e047110bae2578
urbanyoung <urbanyoungnz@gmail.com>
2012-07-11 21:02:58 +1200

    SQLite stuff

--- sql_test/main.cpp
+++ sql_test/main.cpp
@@ -1,11 +1,41 @@
 #include <string>
 #include <stdio.h>
 
+class A
+{
+public:
+	virtual ~A(){
+		printf("%s\n", __FUNCTION__);
+	}
+
+};
+
+class B : public A
+{
+public:
+	virtual ~B(){
+		printf("%s\n", __FUNCTION__);
+	}
+};
+
+class C : public B
+{
+public:
+	virtual ~C()
+	{
+		printf("%s\n", __FUNCTION__);
+	}
+
+};
 int main()
 {
-	std::string s = "test";
-	char* cstr = (char*)s.c_str();
-	cstr[0] = 'p';
-	printf("String: %s\nc_str: %s\n", s.c_str(), cstr);
+	C* c = new C();
+	A* cast = (A*)c;
+	delete cast;
+
+	C* c_ = new C();
+	delete c_;
+	
+
 	return 0;
 }
--- sql_test/sqlitepp.cpp
+++ sql_test/sqlitepp.cpp
@@ -1,86 +1,45 @@
-#pragma once
+#include "sqlitepp.h"
 
-#include <string>
-#include <vector>
-
-/* Simple SQLite wrapper supporting prepared parameter binding for select
- * and insert operations. 
- */
-namespace sqlite 
+namespace sqlite
 {
-	class SQLiteRow;
-	class SQLiteTypeError;
-	class SQLiteBinding;
-
-	class SQLite
+	//-----------------------------------------------------------------------------------------
+	// Class: SQLiteError
+	//
+	SQLiteError::SQLiteError(SQLPP_ERRCODE err) : std::exception() 
 	{
-	public:
-		SQLite(std::string path_to_db);
-		~SQLite();
+		this->err = err;
+		switch (err)
+		{
+		case SQL_ERRCODE_TYPE:
+			{
+				desc = "SQLiteTypeError - Attempted to access unexpected data type.";
+			} break;
+		}		
+	}
 
-		/* Execute a prepared select query using bound parameters
-		 * no good way to do this without C++0x :/ */
-		typedef SQLiteBinding sqlb;
-		std::vector<SQLiteRow*> Select(const wchar_t* statement);
-		std::vector<SQLiteRow*> Select(const wchar_t* statement,
-			sqlb b1);
-		std::vector<SQLiteRow*> Select(const wchar_t* statement,
-			sqlb b1, sqlb b2);
-		std::vector<SQLiteRow*> Select(const wchar_t* statement,
-			sqlb b1, sqlb b2, sqlb b3);
-		std::vector<SQLiteRow*> Select(const wchar_t* statement,
-			sqlb b1, sqlb b2, sqlb b3, sqlb b4);
-		std::vector<SQLiteRow*> Select(const wchar_t* statement,
-			sqlb b1, sqlb b2, sqlb b3, sqlb b4, sqlb b5);
-		std::vector<SQLiteRow*> Select(const wchar_t* statement,
-			sqlb b1, sqlb b2, sqlb b3, sqlb b4, sqlb b5, sqlb b6);
-		std::vector<SQLiteRow*> Select(const wchar_t* statement,
-			sqlb b1, sqlb b2, sqlb b3, sqlb b4, sqlb b5, sqlb b6, sqlb b7);
-
-
-
-		void sql_free(SQLiteRow* obj);		
-	};
-
-	class SQLiteBinding 
+	//-----------------------------------------------------------------------------------------
+	// Class: SQLite
+	//
+	SQLite::SQLite(std::string path_to_db)
 	{
-	public:
-		SQLiteBinding(int val);
-		SQLiteBinding(std::string val);
-		SQLiteBinding(std::wstring val);
-		SQLiteBinding(const char* val);
-		SQLiteBinding(const wchar_t* val);
-		SQLiteBinding(double val);
-		SQLiteBinding(float val);
-		SQLiteBinding(bool val);
-		~SQLiteBinding();
-	};
 
-	class SQLiteRow 
+	}
+
+	SQLite::~SQLite()
 	{
-	private: 
-		SQLiteRow();
-		~SQLiteRow();
-
-	public:
-		
-		/*	Get the row data in various data types, if the requested type
-		 *	is not stored in this row a SQLiteTypeError exception is thrown
-		 */
-		const std::string GetStr();
-		const std::wstring WGetStr();
-		const int GetInt();
-		const double GetDouble();
-		const float GetFloat();
-		const bool GetBool();
-
-		friend class SQLite;
-	};
-
-	class SQLiteTypeError : public std::exception
+		std::set<SQLiteObject*>::iterator itr = allocated_objects.begin();
+		while (itr != allocated_objects.end()) {
+			delete *itr;
+			itr = allocated_objects.erase(itr);
+		}
+	}
+	
+	void SQLite::free_object(SQLiteObject* obj)
 	{
-	public:
-		SQLiteTypeError(const char* excp);
-		virtual const char* what() const throw();
-	};
+		std::set<SQLiteObject*>::iterator itr = allocated_objects.find(obj);
+		if (itr != allocated_objects.end()) {
+			allocated_objects.erase(itr);
+			delete obj;
+		}
+	}
 }
