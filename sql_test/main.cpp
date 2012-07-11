b59ed33f387df1860def964999b6a2872d7e6614
urbanyoung <urbanyoungnz@gmail.com>
2012-07-11 22:06:36 +1200

    SQLiteQuery outline

--- sql_test/main.cpp
+++ sql_test/main.cpp
@@ -21,21 +21,33 @@ public:
 class C : public B
 {
 public:
+	C() {}
+	C(int) {}
 	virtual ~C()
 	{
 		printf("%s\n", __FUNCTION__);
 	}
-
 };
+
+void test(C c, C d)
+{
+
+}
+
 int main()
 {
+	C c;
+	test(c, NULL);
+	/*
 	C* c = new C();
 	A* cast = (A*)c;
+	printf("Deleting casted value\n");
 	delete cast;
 
+	printf("Deleting typed value\n");
 	C* c_ = new C();
 	delete c_;
-	
+	*/
 
 	return 0;
 }
