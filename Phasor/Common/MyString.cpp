7233b545816bd3b2039cfc953a38bfbac3b99429
urbanyoung <urbanyoungnz@gmail.com>
2012-12-05 15:01:25 +1300

    templated TokenizeArgs, removed old code.

--- Phasor/Common/MyString.cpp
+++ Phasor/Common/MyString.cpp
@@ -78,74 +78,17 @@ std::string m_sprintf(const char *fmt, ...)
 	va_end(ArgList);
 	return str;
 }
+
+// Used in TokenizeArgs
 template <>
 const std::string ArgsSearchString<std::string>()
 {
 	return std::string("\"' ");
 }
 
+// Used in TokenizeArgs
 template <>
 const std::wstring ArgsSearchString<std::wstring>()
 {
 	return std::wstring(L"\"' ");
 }
-// Get the substring ending at the next occurrence of c.
-// start is the position (inclusive) where to start searching from.
-// end is the position after the next occurrence, or npos if none.
-/*template <class T, class _Tc>
-T GetStringEndingAtNext(const T& input, _Tc c, size_t start, size_t& end)
-{
-	size_t found = input.find_first_of(c, start);
-	T out = input.substr(start, found - start);
-	end = found == input.npos ? input.npos : found + 1;
-	return out;
-}
-
-std::vector<std::string> TokenizeArgs(const std::string& in)
-{
-	using namespace std;
-	vector<string> out;
-	const string tofind = "\"' "; // " ' or space
-
-	size_t curpos = 0;
-	while (curpos != in.npos)
-	{
-		curpos = in.find_first_not_of(' ', curpos);
-		if (curpos == in.npos) break;
-		size_t nextpos = in.find_first_of(tofind, curpos);
-		if (nextpos == in.npos) { // no more matches, copy everything.
-			out.push_back(in.substr(curpos, in.npos));
-			break;
-		}
-		char c = in.at(nextpos);
-		size_t startfrom = c == ' ' ? curpos : curpos + 1;
-		string token = GetStringEndingAtNext<string, char>(in, c, startfrom, curpos);	
-		if (token.size()) out.push_back(token);
-	}
-	return out;
-}
-
-std::vector<std::wstring> TokenizeWArgs(const std::wstring& in)
-{
-	using namespace std;
-	vector<wstring> out;
-	const wstring tofind = L"\"' "; // " ' or space
-
-	size_t curpos = 0;
-	while (curpos != in.npos)
-	{
-		curpos = in.find_first_not_of(' ', curpos);
-		if (curpos == in.npos) break;
-		size_t nextpos = in.find_first_of(tofind, curpos);
-		if (nextpos == in.npos) { // no more matches, copy everything.
-			out.push_back(in.substr(curpos, in.npos));
-			break;
-		}
-		wchar_t c = in.at(nextpos);
-		size_t startfrom = c == ' ' ? curpos : curpos + 1;
-		wstring token = GetStringEndingAtNext<wstring, wchar_t>(in, c, startfrom, curpos);	
-		if (token.size()) out.push_back(token);
-	}
-	return out;
-}
-*/
