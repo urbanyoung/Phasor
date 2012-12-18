ffd2e0a6a1cf9d869d682d7dc3c6951569c90219
urbanyoung <urbanyoungnz@gmail.com>
2012-12-18 16:48:16 +1300

    tested the mapcycle system, seems to work (scripts too).

---
+++ Phasor/Phasor/Halo/Server/ServerStreams.cpp
@@ -0,0 +1,62 @@
+#include "ServerStreams.h"
+#include "../Addresses.h"
+#include <iostream>
+
+halo::server::CHaloPrintStream g_PrintStream;
+
+namespace halo { namespace server 
+{
+	bool CHaloPrintStream::Write(const std::wstring& str)// str usually has endl appended
+	{
+		if (str.size() == 0) return true;
+		
+		bool ready = *(bool*)UlongToPtr(ADDR_CONSOLEREADY);
+		if (!ready) {
+			std::wcout << str;
+			return true;
+		}
+		// Prepare for writing the string
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		DWORD written = 0;
+
+		CONSOLE_SCREEN_BUFFER_INFO info;
+		SHORT oldX = 0; // used to set cursor back to old position
+
+		// Get current console position info
+		GetConsoleScreenBufferInfo(hConsole, &info);
+		oldX = info.dwCursorPosition.X;
+
+		// Set cursor to start of the last row (where we want to start writing)
+		info.dwCursorPosition.X = 0;
+		info.dwCursorPosition.Y = 299;
+		SetConsoleCursorPosition(hConsole, info.dwCursorPosition);
+
+		FillConsoleOutputCharacterA(hConsole, ' ', 95, info.dwCursorPosition, &written);
+		FillConsoleOutputAttribute(hConsole, 7, 95, info.dwCursorPosition, &written);
+
+		// Write the text
+		WriteConsoleW(hConsole, str.c_str(), str.size(), &written, NULL);
+		//WriteConsoleW(hConsole, L"\n", 1, &written, NULL);
+
+		// Get the current text in the console
+		LPBYTE ptr = (LPBYTE)ADDR_CONSOLEINFO;
+
+		if (*ptr != 0) {
+			// Build current command input
+			std::string formatted = "halo( ";
+
+			formatted += (char*)UlongToPtr(*(DWORD*)ptr + OFFSET_CONSOLETEXT); // current text
+
+			// Rewrite the data to console
+			GetConsoleScreenBufferInfo(hConsole, &info);
+			FillConsoleOutputCharacterA(hConsole, ' ', 95, info.dwCursorPosition, &written);
+			WriteConsoleOutputCharacterA(hConsole, formatted.c_str(), formatted.size(), info.dwCursorPosition, &written);
+
+			// Set the cursor to its old position
+			GetConsoleScreenBufferInfo(hConsole, &info);
+			info.dwCursorPosition.X = oldX;
+			SetConsoleCursorPosition(hConsole, info.dwCursorPosition);
+		}
+		return true;
+	}
+}}
