2abad99b1ff99922d1f17400d6e12c77e5de6e8f
urbanyoung <urbanyoungnz@gmail.com>
2013-02-26 14:59:59 +1300

    fixed #1 (three commits ago)

--- Phasor/Phasor/Halo/Game/Game.cpp
+++ Phasor/Phasor/Halo/Game/Game.cpp
@@ -197,6 +197,7 @@ namespace halo { namespace game {
 
 		bool b = scripting::events::OnWeaponAssignment(player, owningObjectId, order, weap_id,
 			result_id);
+
 		
 		if (!b) return weap_id;
 		return result_id;
