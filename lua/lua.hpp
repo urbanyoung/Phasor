e27075bf823f06272e598fbab81994592a293205
urbanyoung <urbanyoungnz@gmail.com>
2013-07-04 20:21:55 +1200

    fixed lua issues, the library is compiled as C++ but with C name mangling (well, lack thereof) so that Lua modules will work correctly

--- lua/lua.hpp
+++ lua/lua.hpp
@@ -1,8 +1,8 @@
 // lua.hpp
 // Lua header files for C++
-// <<extern "C">> not supplied automatically because Lua also compiles as C++
-
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
-
+// We compile Lua as C++ but don't want its name mangling
+extern "C" {
+	#include "lua.h"
+	#include "lualib.h"
+	#include "lauxlib.h"
+};
--- lua/luaconf.h
+++ lua/luaconf.h
@@ -143,7 +143,7 @@
 #if defined(LUA_BUILD_AS_DLL)	/* { */
 
 #if defined(LUA_CORE) || defined(LUA_LIB)	/* { */
-#define LUA_API __declspec(dllexport)
+#define LUA_API extern "C" __declspec(dllexport)
 #else						/* }{ */
 #define LUA_API __declspec(dllimport)
 #endif						/* } */
